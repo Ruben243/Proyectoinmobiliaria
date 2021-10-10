715e2407ce742c52a9e08e51f21f65442474cce5
Ruben243 <rubengines243@gmail.com>
2021-10-10 11:18:13 +0200

    database

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,8 +1,7 @@
 <?php
 
 function conectarDB(): mysqli {
-    $db = mysqli_connect('eu-cdbr-west-01.cleardb.com
-', 'ba447d123d76ff', '1900ef98', 'heroku_fe37d45214b15de');
+    $db = mysqli_connect('eu-cdbr-west-01.cleardb.com', 'ba447d123d76ff', '1900ef98', 'heroku_fe37d45214b15de');
 
     if (!$db) {
         echo "Error fallo al conectar";
