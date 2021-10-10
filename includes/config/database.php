5a61678093ccc624dc7581816ccb474547e4754f
Ruben243 <rubengines243@gmail.com>
2021-10-10 11:16:40 +0200

    database

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,8 @@
 <?php
 
 function conectarDB(): mysqli {
-    $db = mysqli_connect('localhost:3306', 'ruben', 'Cuelebre243*', 'bienesRaices');
+    $db = mysqli_connect('eu-cdbr-west-01.cleardb.com
+', 'ba447d123d76ff', '1900ef98', 'heroku_fe37d45214b15de');
 
     if (!$db) {
         echo "Error fallo al conectar";
